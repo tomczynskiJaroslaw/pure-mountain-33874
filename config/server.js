944d897368cd15348a7ee6fbe34a571eb77c75be
Jarek <tomczynskijaroslaw@gmail.com>
2020-09-01 11:45:14 +0200

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ff5ea0bf2428814796edc57cdb3490e0'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e65713d3-6c4f-4187-82bf-658e801e4100'
+};
